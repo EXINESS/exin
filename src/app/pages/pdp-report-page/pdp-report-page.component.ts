dc0959b79d6b92ad72b35fdeba9a8c4e6dcb87b8
alirrah <alir.rah81@gmail.com>
2024-02-20 20:58:24 +0330

    [ADD] report work

--- src/app/pages/pdp-report-page/pdp-report-page.component.ts
+++ src/app/pages/pdp-report-page/pdp-report-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {menuItem} from "../../component/models/menuItem";
 
 @Component({
@@ -6,10 +6,51 @@ import {menuItem} from "../../component/models/menuItem";
   templateUrl: './pdp-report-page.component.html',
   styleUrl: './pdp-report-page.component.scss'
 })
-export class PdpReportPageComponent {
+export class PdpReportPageComponent implements OnInit {
+  constructor() {
+  }
+
   public menu: menuItem[] = [
-    {id: 0, title: 'گزارش گیری', link: '/pdp/report'}
+    {id: 0, title: 'میزکار', link: '/pdp/desktop'}
   ];
 
   public activeNumber: number = 0;
+  identify: any;
+  works: { id: number, title: string, subtask: { id: number, title: string, state: number }[] }[] = [];
+  showlist: boolean[] = [];
+  taskNumber:{total: number, success: number}[] = [];
+
+  ngOnInit(): void {
+    let w = localStorage.getItem('works');
+    if (w == null)
+      return;
+    this.works = JSON.parse(w);
+    for (let i in this.works) {
+      this.showlist.push(false);
+      let s= 0;
+      for(let j = 0; j < this.works[i].subtask.length; j++){
+        console.log(j)
+        if(this.works[i].subtask[j].state == 1){
+          s++;
+        }
+      }
+      let total = this.works[i].subtask.length;
+      this.taskNumber.push({total: total, success: s});
+    }
+  }
+
+
+  show(id: number) {
+    this.showlist[id] = !this.showlist[id];
+  }
+
+
+  changeState(id: number, id2: number) {
+    this.works[id].subtask[id2].state++;
+    if(this.works[id].subtask[id2].state > 2){
+      this.works[id].subtask[id2].state = 0;
+    }
+    localStorage.setItem('works', JSON.stringify(this.works));
+  }
 }
+

e28c77255225023fc99aa01ad58bcc94d540f079
alirrah <alir.rah81@gmail.com>
2024-05-25 21:28:12 +0330

    landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {menuItem} from "../../component/models/menuItem";
 
 @Component({
@@ -6,12 +6,35 @@ import {menuItem} from "../../component/models/menuItem";
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+
+export class LandingPageComponent implements OnInit, OnDestroy {
+  @ViewChild('boxOne') boxOne!: ElementRef;
+  @ViewChild('boxTwo') boxTwo!: ElementRef;
+
+  private intervalId: any;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.renderer.setStyle(this.boxOne.nativeElement, 'inset-inline-start', Math.floor(Math.random() * 90) + 'vw');
+      this.renderer.setStyle(this.boxOne.nativeElement, 'inset-block-end', Math.floor(Math.random() * 90) + 'vh');
+
+      this.renderer.setStyle(this.boxTwo.nativeElement, 'inset-inline-start', Math.floor(Math.random() * 90) + 'vw');
+      this.renderer.setStyle(this.boxTwo.nativeElement, 'inset-block-start', Math.floor(Math.random() * 90) + 'vh');
+    }, 390);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
   public menu: menuItem[] = [
     {id: 0, title: "اشتراک ویژه", link: '/'},
     {id: 1, title: "افزونه‌ها", link: '/'},
     {id: 2, title: "آموزش", link: '/'},
     {id: 3, title: "درباره ما", link: '/'},
     {id: 4, title: "درخواست مشاوره", link: '/'},
-  ]
+  ];
 }
--- src/app/pages/pdp-desktop-page/pdp-desktop-page.component.ts
+++ src/app/pages/pdp-desktop-page/pdp-desktop-page.component.ts
@@ -22,7 +22,7 @@ export class PdpDesktopPageComponent implements OnInit {
     saveId: number = 0;
 
     ngOnInit(): void {
-        let w = localStorage.getItem('works');
+        let w: string | null = localStorage.getItem('works');
         if (w == null)
             return;
         this.works = JSON.parse(w);
@@ -31,7 +31,7 @@ export class PdpDesktopPageComponent implements OnInit {
         }
     }
 
-    addtask(title: string) {
+    addtask(title: string): void {
         if (title == '') {
             alert("عنوان اقدام خالی نمی تواند باشد");
             return;
@@ -45,12 +45,12 @@ export class PdpDesktopPageComponent implements OnInit {
         localStorage.setItem('works', JSON.stringify(this.works));
     }
 
-    show(id: number) {
+    show(id: number): void {
         this.showlist[id] = !this.showlist[id];
     }
 
-    addsubtask(title: string) {
-        let id = 0;
+    addsubtask(title: string): void {
+        let id: number = 0;
         if (this.works[this.saveId].subtask.length != 0) {
             id = this.works[this.saveId].subtask[this.works[this.saveId].subtask.length - 1].id + 1;
         }
@@ -61,16 +61,16 @@ export class PdpDesktopPageComponent implements OnInit {
         this.showDialog = false;
     }
 
-    showdialog(id: number) {
+    showdialog(id: number): void {
         this.saveId = id;
         this.showDialog = true;
     }
 
-    notShow() {
+    notShow(): void {
         this.showDialog = false;
     }
 
-    changeState(id: number, id2: number) {
+    changeState(id: number, id2: number): void {
         this.works[id].subtask[id2].state++;
         if(this.works[id].subtask[id2].state > 2){
             this.works[id].subtask[id2].state = 0;
@@ -78,9 +78,9 @@ export class PdpDesktopPageComponent implements OnInit {
         localStorage.setItem('works', JSON.stringify(this.works));
     }
 
-    removeItem(id: number) {
-        let index = -1;
-        for(let i=0; i < this.works.length; i++){
+    removeItem(id: number): void {
+        let index: number = -1;
+        for(let i: number=0; i < this.works.length; i++){
             if(this.works[i].id == id){
                 index = i;
             }
